9a05f8979fc0a2b893bf7a68e10c8c11ffecac79
fallnomega <fallnomega@gmail.com>
2023-02-27 11:39:48 -0800

    UI formatting, updating question generation

--- ui.py
+++ ui.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 import data
-import quiz_brain
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class TriviaUi:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.trivia = quiz_brain
         self.window = tk.Tk()
         self.window.title("Trivia")
         self.window.config(padx=20, pady=20, background=THEME_COLOR, width=400, height=500)
@@ -14,7 +15,7 @@ class TriviaUi:
         self.score.grid(row=0, column=1)
         self.question_area = tk.Canvas(width=300, height=250,bg='white')
         self.question_area.grid(row=1, column=0,pady=50,columnspan=2)
-        self.question = self.question_area.create_text(150, 125, text=f"Question goes here {data.question_data[0]['question']}",
+        self.question = self.question_area.create_text(150, 125, text=f"Question here", width=280,
                                              font=("Arial", 20, 'italic'),fill=THEME_COLOR)
         true_img = tk.PhotoImage(file="images/true.png")
         self.right_button = tk.Button(image=true_img,borderwidth=0,highlightthickness=0)
@@ -23,4 +24,10 @@ class TriviaUi:
         self.wrong_button = tk.Button(image=false_img, borderwidth=0,highlightthickness=0)
         self.wrong_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.trivia.next_question()
+        self.question_area.itemconfig(self.question,text=q_text)
